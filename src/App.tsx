1cd83949f2224cad170c2ed41c650b61fc17c3df
Jakub Kowalczyk <jakubkowalczykpl@gmail.com>
2023-11-11 12:14:15 +0100

    swipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 const style = document.createElement('style');
@@ -69,23 +69,25 @@ function Dog(props: { msg: string, backgroundPosition: string }) {
 }
 
 function Button(props: { style: Partial<CSSStyleDeclaration>, onClick: () => void }) {
-  return <div onClick={props.onClick} style={{
-      width: '50px',
-      height: '50px',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      border: '4px solid #fff',
-      background: '#0000cc',
-      borderRadius: '100%',
-      position: 'fixed',
-      zIndex: 1,
-      ...props.style,
-  } as any}/>
+  const style = Object.assign({
+    width: '50px',
+    height: '50px',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    border: '4px solid #fff',
+    background: '#0000cc',
+    borderRadius: '100%',
+    position: 'fixed',
+    zIndex: 1,
+}, props.style);
+
+  return <div onClick={props.onClick} style={style as any}/>
 }
 
 function App() {
   const [index, setIndex] = useState(0);
   const [isDogVisible, setIsDogVisible] = useState(true);
+  const ref = useRef<HTMLDivElement>(null);
 
   const dogs = [
       { backgroundPosition: '-20px -171px', msg: 'rabul spieszy z pomocą' },
@@ -98,30 +100,229 @@ function App() {
       { backgroundPosition: '-606px -326px', msg: 'czejs się tym zajmie' },
   ];
 
+  const prev = () => {
+    setIndex(i=> {
+      if (i > 0) {
+        refreshDog();
+        return i-1;
+      };
+      return i;
+    });
+  }
+
+  const next = () => {     
+    setIndex(i=> {
+      if (dogs[i+1]) {
+        refreshDog();
+        return i+1;
+      };
+      return i;
+    });
+  }
+
   const refreshDog = () => {
     setIsDogVisible(false);
     setTimeout(() => setIsDogVisible(true));
   };
 
+  useEffect(() => {
+    const listener = new SwipeListener({
+      element: ref.current!,
+      onSwipeLeft: prev,
+      onSwipeRight: next,
+    });
+
+    listener.activate();
+
+    return () => listener.deactivate();
+  }, []);
+
   return (
-    <div style={{
+    <div ref={ref} style={{
       width: '100vw',
       height: '100vh',
       background: 'linear-gradient(135deg, #333, #000) #000',
     }}>
       {isDogVisible && <Dog {...dogs[index]}/>}
-      {index > 0 && <Button style={{ left: '20px' }} onClick={() => {
-        if (index > 0) {
-          setIndex(i=>i-1);
-          refreshDog();
-        }
-      }}/>}
-      {dogs[index+1] && <Button style={{ right: '20px' }} onClick={() => {
-        if (dogs[index+1]) {
-          setIndex(i=>i+1);
-          refreshDog();
-        }
-      }}/>}
+      {index > 0 && <Button style={{ left: '20px' }} onClick={prev}/>}
+      {dogs[index+1] && <Button style={{ right: '20px' }} onClick={next}/>}
     </div>
   );
+}
+
+const touchSupport = 1 || 'ontouchstart' in window || (navigator as any).msMaxTouchPoints;
+
+class Point {
+  x: number;
+  y: number;
+
+  constructor({ x, y }: { x?: number, y?: number } = {}) {
+      this.x = x || 0;
+      this.y = y || 0;
+  }
+
+  add<T extends { x: number, y: number }>(point: T) {
+      return new Point({
+          x: this.x + point.x,
+          y: this.y + point.y,
+      });
+  }
+
+  subtract<T extends { x: number, y: number }>(point: T) {
+      return this.add({
+          x: -point.x,
+          y: -point.y,
+      });
+  }
+}
+
+interface DragListenerParams {
+  element: HTMLElement;
+  onMove?: OnMove;
+  preventDefault?: boolean;
+}
+
+interface OnMove {
+  (p: { drag: Point, move: Point }): void;
+}
+
+class DragListener {
+  private element: HTMLElement;
+  private onMove: OnMove;
+  private startPoint?: Point;
+  private prevPosition?: Point;
+  private active?: boolean;
+
+  constructor({element, onMove = () => {}, preventDefault = true}: DragListenerParams) {
+      this.element = element;
+      this.onMove = onMove;
+      this.onTouchMove = this.onTouchMove.bind(this);
+      this.onMouseMove = this.onMouseMove.bind(this);
+      this.onTouchStart = this.onTouchStart.bind(this);
+      this.onMouseDown = this.onMouseDown.bind(this);
+      this.end = this.end.bind(this);
+      if (!preventDefault) {
+          this.preventDefault = () => {};
+      }
+  }
+
+  activate() {
+      if (!this.active) {
+          this.active = true;
+          this.element.addEventListener(
+              touchSupport ? 'touchstart' : 'mousedown',
+              (touchSupport ? this.onTouchStart : this.onMouseDown) as any
+          );
+      }
+  }
+
+  deactivate() {
+      if (this.active) {
+          this.active = false;
+          this.element.removeEventListener(
+              touchSupport ? 'touchstart' : 'mousedown',
+              (touchSupport ? this.onTouchStart : this.onMouseDown) as any
+          );
+      }
+  }
+
+  end() {
+      this.element.removeEventListener(
+          touchSupport ? 'touchmove' : 'mousemove',
+          (touchSupport ? this.onTouchMove : this.onMouseMove) as any
+      );
+      (touchSupport ? ['touchend'] : ['mouseleave', 'mouseup']).forEach(event => {
+          this.element.removeEventListener(event, this.end);
+      });
+  }
+
+  private onTouchStart(event: TouchEvent) {
+      const touch = event.touches[0];
+
+      this.preventDefault(event);
+      this.start(new Point({ x: touch.pageX, y: touch.pageY }));
+  }
+
+  private onMouseDown (event: MouseEvent) {
+      this.preventDefault(event);
+      this.start(new Point({ x: event.pageX, y: event.pageY }));
+  }
+
+  private start(point: Point) {
+      this.startPoint = point;
+      this.prevPosition = point;
+      this.element.addEventListener(
+          touchSupport ? 'touchmove' : 'mousemove',
+          (touchSupport ? this.onTouchMove : this.onMouseMove) as any
+      );
+      (touchSupport ? ['touchend'] : ['mouseleave', 'mouseup']).forEach(event => {
+          this.element.addEventListener(event, this.end);
+      });
+  }
+
+  private onTouchMove(event: TouchEvent) {
+      const touch = event.touches[0];
+
+      this.preventDefault(event);
+      this.move(new Point({ x: touch.pageX, y: touch.pageY }));
+  }
+
+  private onMouseMove(event: MouseEvent) {
+      this.preventDefault(event);
+      this.move(new Point({ x: event.pageX, y: event.pageY }));
+  }
+
+  private move(point: Point) {
+      if (this.startPoint && this.prevPosition)
+      this.onMove({
+          drag: point.subtract(this.startPoint),
+          move: point.subtract(this.prevPosition),
+      });
+      this.prevPosition = point;
+  }
+
+  private preventDefault<T extends Event>(event: T) {
+      event.preventDefault();
+  }
+}
+
+export interface SwipeListenerParams {
+  element: HTMLElement;
+  onSwipeLeft?: Function;
+  onSwipeRight?: Function;
+}
+
+class DraggableListener extends DragListener{}
+
+class SwipeListener {
+  private onSwipeLeft: Function;
+  private onSwipeRight: Function;
+  private draggableListener: DraggableListener;
+
+  constructor({element, onSwipeRight = () => {}, onSwipeLeft = () => {}}: SwipeListenerParams) {
+      this.onSwipeLeft = onSwipeLeft;
+      this.onSwipeRight = onSwipeRight;
+      this.draggableListener = new DraggableListener({
+          element,
+          preventDefault: false,
+          onMove: ({ drag, move }) => {
+              if (drag.x > 100) {
+                  this.onSwipeRight();
+                  this.draggableListener.end();
+              }
+              else if (drag.x < -100) {
+                  this.onSwipeLeft();
+                  this.draggableListener.end();
+              }
+          }
+      });
+  }
+
+  activate() {
+      this.draggableListener.activate();
+  }
+
+  deactivate() {
+      this.draggableListener.deactivate();
+  }
 }
